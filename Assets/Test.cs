da03c91eb58ccbc08b6816128048b1548748aa0a
ANGELICA <joc9117@gmail.com>
2023-06-12 23:06:55 +0900

    이런이런

---
+++ Assets/Test.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //양심은 있어야지 공부를 한게 있어야 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
