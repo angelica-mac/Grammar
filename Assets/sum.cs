5a578b624f24d8599fc2740b0b275f3c3110731d
ANGELICA <joc9117@gmail.com>
2023-06-13 16:52:05 +0900

    summary

--- Assets/sum.cs
+++ Assets/sum.cs
@@ -2,24 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
-{
-    // 1 + x = 101
-    // 숫자1 상수
-    // x 변수
-    int x = 100; 
+public class sum : MonoBehaviour
+{   
+    int x = 100;
+    int y = 100;
+    int summary;
+
     // Start is called before the first frame update
     void Start()
     {
-        print(x);
+        summary = x + y;
 
-        x = -500;
+        print(summary);
 
-        print(x);
-
-        x = x -500;
-
-        print(x);
     }
 
     // Update is called once per frame
