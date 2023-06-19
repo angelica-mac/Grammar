555f90cf9a5b3d5c627baa02f25af85dc9a56921
ANGELICA <joc9117@gmail.com>
2023-06-19 16:21:49 +0900

    제목변경

--- Assets/Part1D.cs
+++ Assets/Part1D.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sum3 : MonoBehaviour
+public class Part1D : MonoBehaviour
 {
     int a = 100;
     int c;
