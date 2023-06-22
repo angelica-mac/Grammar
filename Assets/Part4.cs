8c77d9add4b559f9eec721f352f907fcb14359de
ANGELICA <joc9117@gmail.com>
2023-06-22 16:42:44 +0900

    지정자

---
+++ Assets/Part4.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Part4 : MonoBehaviour
+{
+    int a = 5; // 멤버 변수, 전역 변수
+
+    void Abc()
+    {
+        a = 6;
+
+        int b = 5; // 지역 변수
+        print(b);
+    }
+
+    void Abc2()
+    {
+        int b = 5; // 지역 변수
+        print(b);
+    }
+}
