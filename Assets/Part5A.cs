3e3b5adb1ead31a762b21983118cfd3b49819d4a
ANGELICA <joc9117@gmail.com>
2023-06-26 17:54:45 +0900

    연산자

---
+++ Assets/Part5A.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 연산자
+public class Part5A : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
