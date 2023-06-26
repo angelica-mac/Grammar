0237455fabd7c54ebacd634b2f87859f1ff73cd5
ANGELICA <joc9117@gmail.com>
2023-06-26 17:51:53 +0900

    접근지정자

--- Assets/Part4B.cs
+++ Assets/Part4B.cs
@@ -7,7 +7,7 @@ public class Test
 {
     private int a;  // 변수
     public int b;
-    public static int c; // 공공의 공유자원
+    public static int c; // 공공의 공유자원.정적변수. 공유자원 수정하려면 class자체로 접근해야 함.
 
     public void Abc(){} // 함수에도 적용 가능
     private void Abc2(){}
@@ -18,9 +18,9 @@ public class Part4B : MonoBehaviour
 {
     Test aaa;
 
-    Test a1;
-    Test a2;
-    Test a3;
+    Test a1 = new Test(); // 선언만 하면 오류 생김. 할당해줘야 함.
+    Test a2 = new Test();
+    Test a3 = new Test();
 
     void start()
     {
@@ -36,10 +36,13 @@ public class Part4B : MonoBehaviour
 
         a2.b = 5;
         a1.b = 1;
-        a3.b = 10;
+        a3.b = 10;  // b 각각 다른값이 존재가능.
+
+        Test.c = 100; // class로 접근. 공유자원 수정하려면 class자체로 접근해야 함.
 
         print(a1.b);
         print(a2.b);
         print(a3.b);
+        print(Test.c);
     }
 }
