7ac63ecf3615d36eda44e1ac2227791544d1d8b3
ANGELICA <joc9117@gmail.com>
2023-06-27 10:52:40 +0900

    연산자

--- Assets/Part5A.cs
+++ Assets/Part5A.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 // 연산자
 public class Part5A : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+// 기본연산자 + - * / %(나머지 값) =(대입연산자)
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+int a = 10;
+int b = 3;
+int c = 0;
+
+void start()
+{
+    a += b; // a = a + b 복합대입연산자
+    c = c+1; // c += 1, c++ ++c 1만 더해주고 싶을때 사용
+
+    print(a); // 13
+    print(++c); //1을 먼저 더한값을 출력
+    print(c++); //0을 출력후 다음부터 1을 더함.
+}
 }
