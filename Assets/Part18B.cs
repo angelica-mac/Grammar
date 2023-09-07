1e9bc8a7fbfdbfad525e9aaf02c1b39ae6e89ef0
ANGELICA <joc9117@gmail.com>
2023-09-07 16:50:57 +0900

    람다식

--- Assets/Part18B.cs
+++ Assets/Part18B.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 람다식 델리게이트 형식매개변수 혼합 응용
 public class Part18B : MonoBehaviour
 {
+    delegate void Mydelegate<T>(T a, T b);
+    Mydelegate<int> mydelegate;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mydelegate += ( int a, int b )=> print(a+b);  // 람다식
+        mydelegate(3,5);
     }
 
     // Update is called once per frame
